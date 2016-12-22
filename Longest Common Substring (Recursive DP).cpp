32f7160c3db2fe3f6f66debac1ec8fab504790e6
Abhinav Jha <abhi28.jha@gmail.com>
2016-12-22 06:50:56 +0530

    Initial Commit

---
+++ Longest Common Substring (Recursive DP).cpp
@@ -0,0 +1,58 @@
+/*input
+GeeksforGeeks
+GeeksQuiz
+*/
+
+/*~~~~~~~~~~~~~~~~~~~~dwij28 == Abhinav Jha~~~~~~~~~~~~~~~~~~~~*/
+
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define MAX(a, b) ((a > b) ? a : b)
+#define MIN(a, b) ((a < b) ? a : b)
+#define set_bit(n, x, i) (n ^ ((-x ^ n) & (1 << i)))
+#define get_bit(n, i) ((n >> i) & 1) 
+using namespace std;
+
+template<typename T>void scan(T&x){bool n=false;register T c=getchar();x=0;if(c=='-'){n=true;c=getchar();}for(;(c>47&&c<58);c=getchar()){x=(x<<1)+(x<<3)+c-48;}if(n){x*=-1;}}
+template<typename T>void allocate(T **x,T n){*x=(T*)malloc(sizeof(T)*n);}
+
+int dp[110][110];
+char a[110], b[110];
+
+void solve(int x, int y) {
+	if (x < 0 || y < 0 || dp[x][y] != -1) return;
+	solve(x-1, y-1); solve(x, y-1); solve(x-1, y);
+	dp[x][y] = (a[x] == b[y]) ? 1 + dp[x-1][y-1] : 0;
+}
+
+void printAllLCS(int ans) { // assumes LCS Table has been calculated
+	std::vector< std::pair<int, int> > data; // positions where each LCS end
+	for (int i = 0; i < 100; i++)
+		for (int j = 0; j < 100; j++)
+			if (dp[i][j] == ans)
+				data.pb(mp(i, j));
+	for (int p = 0; p < data.size(); p++) {
+		int x = data[p].first, y = data[p].second;
+		char s[100]; int k = 0;
+		for (; x >= 0 && y >= 0 && dp[x][y] != 0; x--, y--)
+			s[k++] = a[x];
+		s[k] = '\0';
+		std::reverse(s, s+k);
+		printf("%s\n", s);
+	}
+}
+
+int main() {
+	int ans = 0;
+	scanf("%s%s", a, b);
+	memset(dp, -1, sizeof(dp));
+	solve(strlen(a), strlen(b));
+	for (int i = 0, m = strlen(a); i < m; i++)
+		for (int j = 0, n = strlen(b); j < n; j++)
+			ans = MAX(ans, dp[i][j]);
+	printf("%d\n", ans);
+	printAllLCS(ans);
+	return 0;
+}
